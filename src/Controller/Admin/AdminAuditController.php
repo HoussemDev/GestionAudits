f6175509aaf4b9113702be86e3bddbb56468865a
HoussemDev <houss.sword@gmail.com>
2019-02-27 22:55:05 +0100

    Module Audit completed

---
+++ src/Controller/Admin/AdminAuditController.php
@@ -0,0 +1,151 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sony
+ * Date: 27/02/2019
+ * Time: 22:20
+ */
+
+namespace App\Controller\Admin;
+
+
+use App\Entity\Audit;
+use App\Form\AuditType;
+use App\Repository\AuditRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminAuditController extends AbstractController
+{
+
+	/**
+	 * @var AuditRepository
+	 */
+	private $repository;
+	/**
+	 * @var ObjectManager
+	 */
+	private $em;
+
+	public function __construct(AuditRepository $repository, ObjectManager $em)
+	{
+		$this->repository = $repository;
+		$this->em = $em;
+	}
+
+
+	/**
+	 * @Route("/admin/audit", name="admin.auditlist.index")
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function index()
+	{
+		$audits = $this->repository->findAll();
+		return $this->render('Admin/Audit/index.html.twig', compact('audits'));
+	}
+
+	/**
+	 * @Route("/admin/audit/{slug}.{id}", name="audit.show", requirements={"slug": "[a-z0-9\-]*"})
+	 * @param Audit $audit
+	 * @return Response
+	 */
+	public function show(Audit $audit, string $slug):Response
+	{
+		if ($audit->getSlug() !== $slug)
+		{
+			return $this->redirectToRoute('audit.show', [
+				'id' => $audit->getId(),
+				'slug' => $audit->getSlug()
+			], 301);
+		}
+
+		return $this->render('Admin/Audit/show.html.twig', [
+			'audits' => $audit,
+			'current_menu' => 'audit'
+
+		]);
+}
+
+
+	/**
+	 * @Route("/admin/audit/create", name="admin.audit.new")
+	 */
+	public function new(Request $request)
+	{
+		$audit = new Audit();
+		$form = $this->createForm(AuditType::class, $audit);
+		$form->handleRequest($request);
+
+		if($form->isSubmitted() && $form->isValid())
+		{
+			$this->em->persist($audit);
+			$this->em->flush();
+			$this->addFlash('success','Audit Created' );
+
+			return $this->redirectToRoute('admin.auditlist.index');
+		}
+		return $this->render('Admin/Audit/edit.html.twig',  [
+			'audit' => $audit,
+			'form' => $form->createView()
+		]);
+	}
+
+
+
+	/**
+	 * @Route("/admin/audit/{id}", name="admin.audit.delete", methods="DELETE")
+	 * @param Audit $audit
+	 * @param Request $request
+	 * @return \Symfony\Component\HttpFoundation\RedirectResponse
+	 */
+	public function delete(Audit $audit, Request $request)
+	{
+		if ($this->isCsrfTokenValid('delete' . $audit->getId(), $request->get('_token')))
+		{
+			$this->em->remove($audit);
+			$this->em->flush();
+			$this->addFlash('success','Audit removed' );
+
+		}
+
+
+		return $this->redirectToRoute('admin.auditlist.index');
+	}
+
+
+	/**
+	 * @Route("/admin/audit/{id}", name="admin.audit.edit")
+	 * @param Audit $audit
+	 * @param Request $request
+	 * @return \Symfony\Component\HttpFoundation\Response
+	 */
+	public function edit(Audit $audit, Request $request)
+	{
+		$form =$this->createForm(AuditType::class, $audit);
+
+		$form->handleRequest($request);
+
+		if($form->isSubmitted() && $form->isValid())
+		{
+			$this->em->flush();
+			$this->addFlash('success','Audit  Edited' );
+			return $this->redirectToRoute('admin.auditlist.index');
+		}
+
+		return $this->render('Admin/Audit/edit.html.twig',  [
+			'audit' => $audit,
+			'form' => $form->createView() ]);
+	}
+
+
+
+
+
+
+
+
+
+}
---
+++ src/Entity/Audit.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Cocur\Slugify\Slugify;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\AuditRepository")
+ */
+class Audit
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Status;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $Numberfte;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $audittype;
+
+    /**
+     * @ORM\Column(type="string", length=500, nullable=true)
+     */
+    private $scopestatment;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $auditdfinding;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+	public function getSlug(): string
+	{
+		return(new Slugify())->slugify($this->Title);
+	}
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->Status;
+    }
+
+    public function setStatus(string $Status): self
+    {
+        $this->Status = $Status;
+
+        return $this;
+    }
+
+    public function getNumberfte(): ?int
+    {
+        return $this->Numberfte;
+    }
+
+    public function setNumberfte(?int $Numberfte): self
+    {
+        $this->Numberfte = $Numberfte;
+
+        return $this;
+    }
+
+    public function getAudittype(): ?string
+    {
+        return $this->audittype;
+    }
+
+    public function setAudittype(?string $audittype): self
+    {
+        $this->audittype = $audittype;
+
+        return $this;
+    }
+
+    public function getScopestatment(): ?string
+    {
+        return $this->scopestatment;
+    }
+
+    public function setScopestatment(?string $scopestatment): self
+    {
+        $this->scopestatment = $scopestatment;
+
+        return $this;
+    }
+
+    public function getAuditdfinding(): ?string
+    {
+        return $this->auditdfinding;
+    }
+
+    public function setAuditdfinding(?string $auditdfinding): self
+    {
+        $this->auditdfinding = $auditdfinding;
+
+        return $this;
+    }
+}
